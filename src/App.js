c8ec3eac15471a4bc9ab014cbe1b9453c1c747d3
Saumitra Dey <saumitra@moengage.com>
2018-11-24 00:11:41 +0530

    Removed much of code from App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
 
 class App extends Component {
-    ref = React.createRef();
-
-    state = {
-        value: ""
-    };
-
-    submit = () => {
-        alert(`Uncontrolled input value is "${this.ref.current.value}"`);
-    };
-
     render() {
-      let { value } = this.state 
         return (
             <div className="demo master">
-               Nothing here
+              Nothing here, check other branches! 
             </div>
         );
     }
