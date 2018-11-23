028e0411b4d1931610a60b41c2615bfc262ccae8
Saumitra Dey <saumitra@moengage.com>
2018-11-23 10:24:32 +0530

    Added commented solution

--- src/App.js
+++ src/App.js
@@ -9,20 +9,16 @@ share an implicit state that let's them communicate with
 each other in the background.`;
 
 class Dialog extends React.Component {
-    static Header = props => (
-        <div className="dialog-header">{props.children}</div>
-    );
-
-    static Body = props => <div className="dialog-body">{props.children}</div>;
-
-    static Actions = props => (
-        <div className="dialog-actions">{props.children}</div>
-    );
-
     render() {
-        return <div className="dialog">
-            { this.props.children}
-        </div>;
+        return (<div className="dialog">
+        <div className="dialog-header">{dialogHeader}</div>
+        <div className="dialog-body">
+            {dialogContent}
+        </div>
+        <div className="dialog-actions">
+            <button>Close</button>
+        </div>
+    </div>)
     }
 }
 
@@ -31,18 +27,52 @@ class App extends Component {
         return (
             <div className="demo ch-5">
                 <h1 className="title">Compound component</h1>
-                <Dialog>
-                    <Dialog.Header>{ dialogHeader }</Dialog.Header>
-                    <Dialog.Body>
-                        {dialogContent}
-                    </Dialog.Body>
-                    <Dialog.Actions>
-                        <button>Close</button>
-                    </Dialog.Actions>
-                </Dialog>
+                <Dialog />
             </div>
         );
     }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+static Header = props => (
+    <div className="dialog-header">{props.children}</div>
+);
+
+static Body = props => <div className="dialog-body">{props.children}</div>;
+
+static Actions = props => (
+    <div className="dialog-actions">{props.children}</div>
+);
+
+render() {
+    return <div className="dialog">
+        { this.props.children}
+    </div>;
+}
+
+
+
+<Dialog.Header>{ dialogHeader }</Dialog.Header>
+<Dialog.Body>
+    {dialogContent}
+</Dialog.Body>
+<Dialog.Actions>
+    <button>Close</button>
+</Dialog.Actions>
+*/
