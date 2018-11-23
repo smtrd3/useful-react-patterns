de2970cb88e4c444fed3334c8e5669e723eaaa77
Saumitra Dey <saumitra@moengage.com>
2018-11-23 08:44:04 +0530

    Almost complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 import React, { Component } from "react";
 
+// List presentational component
+class List extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id}>
+                        {item.first_name} {item.last_name}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+// container component
+class ListContainer extends React.Component {
+    state = {
+        items: []
+    };
+
+    async componentDidMount() {
+        let res = await fetch(`https://reqres.in/api/users?per_page=10&page=1`);
+        let json = await res.json();
+        this.setState({ items: json.data });
+    }
+
+    render() {
+        return <List items={this.state.items} />;
+    }
+}
+
 class App extends Component {
     ref = React.createRef();
 
@@ -7,18 +39,11 @@ class App extends Component {
         value: ""
     };
 
-    submit = () => {
-        alert(`Uncontrolled input value is "${this.ref.current.value}"`);
-    };
-
     render() {
-      let { value } = this.state 
         return (
             <div className="demo first">
                 <h1 className="title">Container</h1>
-                {/* <code>
-                  { JSON.stringify(this.state, null, 2) }
-                </code> */}
+                <ListContainer />
                 <div>
                     <button onClick={this.submit}>Submit</button>
                 </div>
