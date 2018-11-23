d473370617366d45fe46c61b10a7aa3601ee44b8
Saumitra Dey <saumitra@moengage.com>
2018-11-23 07:56:10 +0530

    Completed first chapter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="demo">
-        Hello!
-      </div>
-    );
-  }
+    ref = React.createRef();
+
+    state = {
+        value: ""
+    };
+
+    submit = () => {
+        alert(`Uncontrolled input value is "${this.ref.current.value}"`);
+    };
+
+    render() {
+      let { value } = this.state 
+        return (
+            <div className="demo first">
+                <h1 className="title">Controlled component</h1>
+                {/* <code>
+                  { JSON.stringify(this.state, null, 2) }
+                </code> */}
+                <div className="uncontrolled-input">
+                    <label>Uncontrolled component</label>
+                    <input placeholder="Uncontrolled" ref={this.ref} />
+                </div>
+                <div className="controlled-input">
+                    <label>Controlled component</label>
+                    <input
+                        placeholder="Controlled"
+                        value={value}
+                        onChange={e => this.setState({ value: e.target.value })}
+                    />
+                </div>
+                <div>
+                    <button onClick={this.submit}>Submit</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
